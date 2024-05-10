d055f8054b8215567fc7aabf7857dff2b955f872
mahdihaghnegar <mahdi.haghnegar@gmail.com>
2024-05-10 19:59:18 +0330

    befor deploy

---
+++ server/testdb.js
@@ -0,0 +1,29 @@
+//const { MongoClient, ServerApiVersion } = require("mongodb");
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri =
+  "mongodb+srv://mahdihaghnegar:ZWvnD6opKbI6KPW7@cluster0.ueoiorp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
