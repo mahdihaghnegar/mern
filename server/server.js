1c61f1e2a763d8a6a7a9f450a19625e8d1b64235
mahdiRMS <mahdi.haghnegar@gmail.com>
2024-05-11 11:38:22 +0430

    server cors frontend option

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // middleware
 const corsOptions = {
-  origin: "http://127.0.0.1:5173", // frontend URI (ReactJS)
+  origin: "https://mern-fe-6cdm.onrender.com", // "http://127.0.0.1:5173", // frontend URI (ReactJS)
 };
 app.use(express.json());
 app.use(cors(corsOptions));
